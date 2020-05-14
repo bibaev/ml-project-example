421debaacbd8bdcdf72d3e694a74f5cdeda8823d
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2020-05-15 00:46:00 +0300

    Add memory for validation

--- .space.kts
+++ .space.kts
@@ -10,5 +10,8 @@ job("Train model") {
     container("tensorflow/tensorflow:2.2.0") {
         entrypoint("python")
         args("/mnt/space/work/src/evaluate.py", "/mnt/space/share/model")
+        resources {
+            memory = 8000
+        }
     }
 }
