cc56a94e8dc2322881f5eaf6c5ff07499b276b17
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2020-05-14 23:58:59 +0300

    Use full name of docker image

--- .space.kts
+++ .space.kts
@@ -1,10 +1,10 @@
 job("Train model") {
-    container("tensorflow:2.2.0") {
+    container("tensorflow/tensorflow:2.2.0") {
         entrypoint("/bin/bash")
         args("train.sh")
     }
 
-    container("tensorflow:2.2.0") {
+    container("tensorflow/tensorflow:2.2.0") {
         entrypoint("/bin/bash")
         args("evaluate.sh")
     }
