ccffd38aed1514a2534a4361a8f1e94b00f9578a
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2020-05-15 12:06:39 +0300

    Set version from automation

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 cp /mnt/space/share/model ./model
-docker build -t registry.jetbrains.team/ml-in-space/mnist-example:0.0.2 .
+docker build -t registry.jetbrains.team/ml-in-space/mnist-example:0.1.$JB_SPACE_EXECUTION_NUMBER .
 docker login registry.jetbrains.team --username $JB_SPACE_CLIENT_ID --password $JB_SPACE_CLIENT_SECRET
-docker push registry.jetbrains.team/ml-in-space/mnist-service:0.0.2
+docker push registry.jetbrains.team/ml-in-space/mnist-service:0.1.$JB_SPACE_EXECUTION_NUMBER
